609f7dea76d324b6d683a6524dc96b948956f0eb
Makarov <makarovdaniil2000@mail.ru>
2020-10-09 16:05:19 +0700

    --

--- minesweeper/src/main/java/ru/kami/minesweeper/view/entity/GameNewEnterJButton.java
+++ minesweeper/src/main/java/ru/kami/minesweeper/view/entity/GameNewEnterJButton.java
@@ -1,13 +1,15 @@
 package ru.kami.minesweeper.view.entity;
 
 import ru.kami.minesweeper.controller.Controller;
-import ru.kami.minesweeper.view.validator.NewGameValueValidator;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Кнопка начала новой игры
+ */
 public class GameNewEnterJButton extends JButton {
     private static final String BUTTON_TITLE = "Run";
     private final Controller controller;
@@ -23,41 +25,20 @@ public class GameNewEnterJButton extends JButton {
         this.height = height;
         this.mines = mines;
         this.owner = owner;
-        setToolTipText("Field size 6-25 Mines 6-25");
     }
 
     {
         addActionListener((new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!width.getText().isEmpty() && !height.getText().isEmpty() && !mines.getText().isEmpty()) {
-                    int gridWidth = Integer.parseInt(width.getText());
-                    int gridHeight = Integer.parseInt(height.getText());
-                    int totalMines = Integer.parseInt(mines.getText());
-                    NewGameValueValidator validator = new NewGameValueValidator();
-                    if (validator.isNotValidGridSize(gridWidth)) {
-                        width.createWarning();
-                        return;
-                    } else {
-                        width.cancelWarning();
-                    }
-                    if (validator.isNotValidGridSize(gridHeight)) {
-                        height.createWarning();
-                        return;
-                    } else {
-                        height.cancelWarning();
-                    }
-                    if (!validator.isValidTotalMines(gridWidth, gridHeight, totalMines)) {
-                        mines.createWarning();
-                        return;
-                    } else {
-                        mines.cancelWarning();
-                    }
+                int gridWidth = Integer.parseInt(width.getText());
+                int gridHeight = Integer.parseInt(height.getText());
+                int totalMines = Integer.parseInt(mines.getText());
                     controller.handleUserClickedOnNewGame(gridWidth, gridHeight, totalMines);
                     owner.setVisible(false);
                     owner.dispose();
                 }
             }
-        }));
+        ));
     }
 }
